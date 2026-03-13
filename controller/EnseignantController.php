c45265805fcad912d4413b947d9139671def9a6b
Gazegagnon <hamidoubance53@gmailcom>
2026-03-13 13:38:45 +0100

    correction EnseignantController.php

--- controller/EnseignantController.php
+++ controller/EnseignantController.php
@@ -6,98 +6,84 @@ use App\Model\EnseignantModel;
 use App\Model\QcmModel;
 use App\Model\QuestionModel;
 use App\Model\ReponseModel;
-use App\Entity\Qcm;
 use App\Entity\Enseignant;
-use App\Entity\Question;
-use App\Entity\Reponse;
-
-
 
 class EnseignantController extends Controller
-
 {
     public function EnseiHttps()
     {
-        $enseignantMdl = new EnseignantModel;
-        $qcmMdl = new QcmModel;
-        $questionMdl = new QuestionModel;
-        $reponseMdl = new ReponseModel;
+        $enseignantMdl = new EnseignantModel();
+        $qcmMdl = new QcmModel();
+        $questionMdl = new QuestionModel();
+        $reponseMdl = new ReponseModel();
 
-        if(isset($_GET['action']))
-        {
+        if (isset($_GET['action'])) {
             $action = $_GET['action'];
 
-            switch($action)
-            {
+            switch ($action) {
                 case "enseignant":
+                    if (isset($_SESSION['enseignant']) && is_object($_SESSION['enseignant'])) {
+                        $enseignant = $_SESSION['enseignant'];
+                        $idEns = $enseignant->getId();
 
-                    if(isset($_SESSION['enseignant']))
-                    {
-                        $idEns = unserialize($_SESSION['enseignant'])->getId();
                         $qcms = $qcmMdl->getQcmByEns($idEns);
                         $questions = $questionMdl->questions($idEns);
                         $reponses = $reponseMdl->reponses();
-                        // var_dump($idEns);
-                        $this->render("enseignant/index", ["qcms" => $qcms, "questions" => $questions, "reponses" => $reponses]);
-                        break;
-                        
-                        
-                    }else{
-                        header("location: ?action=login_ens");
-                        exit;
 
+                        $this->render("enseignant/index", [
+                            "qcms" => $qcms,
+                            "questions" => $questions,
+                            "reponses" => $reponses
+                        ]);
+                        break;
+                    } else {
+                        header("Location: ?action=login_ens");
+                        exit;
                     }
-                    
 
                 case "login_ens":
-                    if(isset($_POST['login_admin']))
-                    {
+                    if (isset($_POST['login_admin'])) {
                         extract($_POST);
-                        $enseignantMdl->login($mail, $mdpEns);
-                        if(isset($_SESSION['enseignant']))
-                        {
-                            header("location: ?action=enseignant");
+
+                        $enseignant = $enseignantMdl->login($mail, $mdpEns);
+
+                        if ($enseignant) {
+                            $_SESSION['enseignant'] = $enseignant;
+                            header("Location: ?action=enseignant");
                             exit;
-
+                        } else {
+                            $erreur = "Email ou mot de passe incorrect.";
+                            $this->render("enseignant/login", ["erreur" => $erreur]);
+                            return;
                         }
-
-
                     }
+
                     $this->render("enseignant/login");
                     break;
 
-
                 case "add_admin":
-                    if(isset($_POST['new_admin']))
-                    {
+                    if (isset($_POST['new_admin'])) {
                         extract($_POST);
 
-                        $enseignant = new Enseignant(0,$nom,$mdp,$mail);
-                        $enseignantMdl->create($enseignant);
-                        if(isset($_SESSION['enseignant']))
-                        {
-                            header("location: ?action=enseignant");
+                        $enseignant = new Enseignant(0, $nom, $mdp, $mail);
+                        $enseignantCree = $enseignantMdl->create($enseignant);
+
+                        if ($enseignantCree) {
+                            $_SESSION['enseignant'] = $enseignantCree;
+                            header("Location: ?action=enseignant");
                             exit;
                         }
-
-
                     }
-             
+
                     $this->render("enseignant/new");
                     break;
-                
+
                 case "logout":
+                    unset($_SESSION['enseignant']);
                     session_destroy();
-                    header("location: ?action=login_ens");
+                    header("Location: ?action=login_ens");
                     exit;
-
-
-
-
-
-
             }
         }
-
     }
 }
