2c4758bf165c7bb991515a2c87935d6bf9c253f0
Gazegagnon <hamidoubance53@gmailcom>
2026-03-13 13:37:04 +0100

    correction EnseignantModel

--- model/EnseignantModel.php
+++ model/EnseignantModel.php
@@ -1,67 +1,66 @@
-<?php 
+<?php
 
 namespace App\Model;
+
 use App\Entity\Enseignant;
 
 class EnseignantModel extends Model
-
 {
     public function create($enseignant)
-
     {
-        $sql = "INSERT INTO `enseignant` VALUES(NULL, :nom, :mdp, :mail)";
+        $sql = "INSERT INTO enseignant VALUES (NULL, :nom, :mdp, :mail)";
+        
         $this->executereq($sql, [
-            "nom" => $enseignant->getNom(),
-            "mdp" => $enseignant->getMotDePasse(),
+            "nom"  => $enseignant->getNom(),
+            "mdp"  => $enseignant->getMotDePasse(),
             "mail" => $enseignant->getEmail()
         ]);
 
         $id = $this->getPdo()->lastInsertId();
 
-        $_SESSION['enseignant'] = serialize( new Enseignant($id,$enseignant->getNom(),$enseignant->getMotDePasse(),$enseignant->getEmail() ));
-        
-
-        return $_SESSION['enseignant'];
-
-
+        return new Enseignant(
+            $id,
+            $enseignant->getNom(),
+            $enseignant->getMotDePasse(),
+            $enseignant->getEmail()
+        );
     }
-
 
     public function login($mail, $mdpEns)
     {
-        $sql = "SELECT * FROM enseignant WHERE  motDePasse = :mdp and email = :email ";
+        $sql = "SELECT * FROM enseignant WHERE motDePasse = :mdp AND email = :email";
         
-        $stmt =$this->executereq($sql, ["mdp" => $mdpEns, "email" => $mail]);
-        // var_dump($stmt);
-        
+        $stmt = $this->executereq($sql, [
+            "mdp"   => $mdpEns,
+            "email" => $mail
+        ]);
+
         $resultat = $stmt->fetch();
 
-        if($resultat)
-        {
-            extract($resultat);
-            $_SESSION['enseignant'] = serialize( new Enseignant($id, $nom,$motDePasse,$email));
-            return $_SESSION['enseignant'];
-
+        if ($resultat) {
+            return new Enseignant(
+                $resultat['id'],
+                $resultat['nom'],
+                $resultat['motDePasse'],
+                $resultat['email']
+            );
         }
 
+        return false;
     }
-
-
-
-    
-
-
 
     public function Enseignants()
     {
         $stmt = $this->getAll("enseignant");
-        $tab = [] ;
+        $tab = [];
 
-        while($resultat = $stmt->fetch())
-        {
-            extract($resultat);
-
-            $tab[] = new Enseignant($id, $nom, $motDePasse, $email);
+        while ($resultat = $stmt->fetch()) {
+            $tab[] = new Enseignant(
+                $resultat['id'],
+                $resultat['nom'],
+                $resultat['motDePasse'],
+                $resultat['email']
+            );
         }
 
         return $tab;
@@ -69,9 +68,18 @@ class EnseignantModel extends Model
 
     public function Enseignant($id)
     {
-        $stmt = $this->getOne("enseignant",$id);
-        extract($stmt->fetch());
-        return new Enseignant($id, $nom, $motDePasse, $email);
+        $stmt = $this->getOne("enseignant", $id);
+        $resultat = $stmt->fetch();
 
+        if (!$resultat) {
+            return false;
+        }
+
+        return new Enseignant(
+            $resultat['id'],
+            $resultat['nom'],
+            $resultat['motDePasse'],
+            $resultat['email']
+        );
     }
 }
